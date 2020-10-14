5304de65603a575cbe63e3129084a1c9626466e3
MuhammadWaqas <mwaqasbashir4@gmail.com>
2020-10-15 00:05:54 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,23 +11,8 @@ export default function App() {
   // count = 1; not possible
   // setCount(2);
   return (
-    <div className={`box ${isMorning ? 'dayLight' : '' }`} >
-      <h1> Have A Good = {isMorning ? 'Morning' : 'Night'}</h1>
-      <br/>
-      <Meage counter={count}/>
-      <br/>
-      <h1>Value of Variable Var Counter is: {count}</h1>
-       <br/>
-       <button onClick={
-         ()=> setCount(++count)
-         }> 
-         Update Counter
-         </button>
-         <button onClick={
-           ()=> setMorning(!isMorning)
-         }>
-           Update Day
-           </button>
+    <div>
+     
        </div>
   );
 }
